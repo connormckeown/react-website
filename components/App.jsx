708abce38a8cd51abd4c1f61a107ff162e58cebf
Connor McKeown <cbmckeown@gmail.com>
2022-05-19 17:18:40 -0700

    removed default code and added styled-component hello world

--- components/App.jsx
+++ components/App.jsx
@@ -1,45 +1,27 @@
-import { useState } from 'react'
-import logo from '../src/logo.svg'
-import './App.css'
+import React from 'react'
+import styled from 'styled-components'
 
-function App() {
-  const [count, setCount] = useState(0)
+const Title = styled.h1`
+  font-size: 1.5em;
+  text-align: center;
+  color: palevioletred;
+`;
 
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.jsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
-    </div>
-  )
+const Wrapper = styled.section`
+  padding: 4em;
+  background: papayawhip;
+`;
+
+export class App extends React.Component {
+  render() {
+    return (
+      <Wrapper>
+        <Title>
+          Hello World!
+        </Title>
+      </Wrapper>
+    )
+  }
 }
 
 export default App
