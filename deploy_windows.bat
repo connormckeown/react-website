6fe73924fc35728fcc0c39a0ef1424f257575e34
Connor McKeown <cbmckeown@gmail.com>
2022-05-28 22:28:44 -0700

    updated the deploy script

--- deploy_windows.bat
+++ deploy_windows.bat
@@ -1,17 +1,10 @@
-
-
-@REM call npm "run" "build"
-@REM cd "dist"
-@REM echo cbmckeown.com > CNAME
-@REM git "init"
-@REM git "checkout" "-b" "gh-pages"
-@REM git "add" "-A"
-@REM git "commit" "-m" "deploy"
-@REM git "push" "-u" "git@github.com:connormckeown/react-website.git" "gh-pages"
-@REM cd ".."
-
+@REM Below builds the site and pushes the contents of the dist folder to a new remote branch 'gh-pages'.
+@REM This means that the remote branch 'gh-pages' will have to be deleted before you can run this script.
 
 call npm "run" "build"
+@REM cd "dist"
+@REM echo "cbmckeown.com" ">" "CNAME"
+@REM cd ".."
 git "add" "dist" "-f"
 git "commit" "-m" "deploy - adding new dist"
 git "subtree" "push" "--prefix" "dist" "origin" "gh-pages"
